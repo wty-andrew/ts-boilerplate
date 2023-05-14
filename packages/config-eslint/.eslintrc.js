f32386ec89497114de2b0df98c6e9aa075f6883a
Andrew <wty.andrew@gmail.com>
2023-05-14 22:56:14 +0800

    :arrow_up: upgrade packages + eslint config update

--- packages/config-eslint/.eslintrc.js
+++ packages/config-eslint/.eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
       files: ['**/*.tsx'],
       rules: {
         'react/prop-types': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
       },
     },
   ],
