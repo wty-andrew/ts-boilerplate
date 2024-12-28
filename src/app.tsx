2176d46792867834d6d4723302f26ef5ffc3a981
Andrew <wty.andrew@gmail.com>
2024-12-28 22:31:09 +0800

    :sparkles: Setup wasm

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
-const App = () => <h1 className="text-3xl font-bold underline">Hello World</h1>
+import { greet } from 'wasm-lib'
+
+const App = () => <h1 className="text-3xl font-bold underline">{greet('World')}</h1>
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 /// <reference types="vitest" />
 
 import path from 'node:path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
+import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
